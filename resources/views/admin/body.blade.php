71241e4a28ece3653be6bb6b72e20a6317142e15
JesseMburu <jesse.mburu@strathmore.edu>
2024-07-16 08:33:48 +0300

    Admin Body

--- resources/views/admin/body.blade.php
+++ resources/views/admin/body.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Your Laravel App</title>
+    <title>Welcome to Nurturing Lives</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
